9a4bcdd129ff101ce9bcfdab01ab521653287f0c
elmyandrean <elmy.andrean@gmail.com>
2022-04-22 22:56:43 +0700

    remove commant in dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,22 +9,12 @@ use App\Models\Patient;
 class DashboardController extends Controller
 {
     public function index(Request $request){
-      $where = [];
-      
-      // if($request->patient_id){
-      //   $where[] = ['patient_id', 'like', '%'.$request->patient_id.'%'];
-      // }
-
-      // \DB::enableQueryLog();
-    
       $patients = Patient::where('name', 'like', '%'.$request->name.'%')
         ->orWhere('id', $request->patient_id)
         ->orWhere('personal_id', 'like', '%'.$request->personal_id.'%')
         ->paginate(10)->withQueryString();
 
-      // $query = \DB::getQueryLog();
-      // dd($query);
-      return view('dashboard.index', [
+        return view('dashboard.index', [
         'patients' => $patients,
       ]);
     }
