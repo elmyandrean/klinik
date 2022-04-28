d2dd8b605a09482724993df8d82c54043be862fc
elmyandrean <elmy.andrean@gmail.com>
2022-04-28 21:22:17 +0700

    add ordering value desc for patient children

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -20,7 +20,7 @@ class Patient extends Model
     ];
 
     public function treatments() {
-      return $this->hasMany(Treatment::class)->orderBy('id');
+      return $this->hasMany(Treatment::class)->orderBy('id', 'DESC');
     }
 
     public function last_treatment() {
