d40333cca5a335a641d8bb817b7acf094f6a657f
elmyandrean <elmy.andrean@gmail.com>
2022-05-04 07:06:50 +0700

    add CUD on diagnoses

--- app/Http/Controllers/DiagnosesController.php
+++ app/Http/Controllers/DiagnosesController.php
@@ -14,4 +14,54 @@ class DiagnosesController extends Controller
         'diagnoses' => $diagnoses,
       ]);
     }
+
+    public function create(){
+      return view('diagnoses.create');
+    }
+
+    public function store(Request $request){
+      $validator = Validator::make($request->all(), [
+        'name' => 'required|min:5',
+      ]);
+
+      if($validator->fails()){
+          return back()->withErrors($validator)->withInput();
+      }
+
+      $validated = $validator->safe()->all();
+
+      Diagnose::insert($validated);
+
+      return redirect()->route('diagnoses.index');
+    }
+
+    public function edit($id) {
+      $diagnose = Diagnose::findOrFail($id);
+
+      return view('diagnoses.edit', [
+        'diagnose' => $diagnose
+      ]);
+    }
+
+    public function update(Request $request, $id) {
+      $validator = Validator::make($request->all(), [
+        'name' => 'required|min:5',
+      ]);
+
+      if($validator->fails()){
+          return back()->withErrors($validator)->withInput();
+      }
+
+      $validated = $validator->safe()->all();
+
+      Diagnose::where('id', $id)->update($validated);
+
+      return redirect()->route('diagnoses.index');
+    }
+
+    public function destroy($id) {
+      Diagnose::where('id', $id)->update(['status' => 0]);
+
+      return redirect()->route('diagnoses.index');
+    }
 }
---
+++ resources/views/diagnoses/edit.blade.php
@@ -0,0 +1,37 @@
+@section('title', 'New Patient')
+
+@extends('template')
+
+@section('content')
+<div class="container pt-3">
+  <div class="row">
+    <div class="col-7">
+      <div class="card">
+        <div class="card-header">
+          Edit Diagnosis
+        </div>
+        <div class="card-body">
+          <form action="{{ route('diagnoses.update', $diagnose->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="row">
+              <div class="mb-3 row">
+                <label for="name" class="col-2 col-form-label">Name <span class="text-danger">*</span></label>
+                <div class="col-8">
+                  <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ $diagnose->name }}" autofocus>
+                  @error('name') <div class="invalid-feedback">{{ $message }}</div> @enderror
+                </div>
+              </div>
+              <div class="mb-3 row">
+                <div class="offset-sm-2 col-sm-8">
+                  <button class="btn btn-primary" type="submit">Save</button>
+                </div>
+              </div>
+            </div>    
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
